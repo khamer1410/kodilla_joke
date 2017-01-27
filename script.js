0369c88d8fa68e31fd15c29fe6916f34d495e29c
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-01-27 18:55:05 +0100

    JQ ajax

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-var url = 'http://api.icndb.com/jokes/random';
-var button = document.getElementById('get-joke');
-var paragraph = document.getElementById('joke');
+// var url = 'http://api.icndb.com/jokes/random';
+// var button = document.getElementById('get-joke');
+// var paragraph = document.getElementById('joke');
 
-button.addEventListener('click', function() {
+// button.addEventListener('click', function() {
+// 	getJoke();
+// });
+
+// function getJoke() {
+// 	var xhr = new XMLHttpRequest();
+// 	xhr.open('GET', url);
+// 	xhr.addEventListener('load', function() {
+// 		var response = JSON.parse(xhr.response);
+// 		paragraph.innerText = response.value.joke;
+// 	});
+// 	xhr.send();
+// }
+
+// getJoke();
+
+//============jQ==================
+
+var url = 'http://api.icndb.com/jokes/random';
+var $button = $('#get-joke').click(function(){
 	getJoke();
 });
+var $paragraph = $('#joke');
 
 function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function() {
-		var response = JSON.parse(xhr.response);
-		paragraph.innerText = response.value.joke;
+	$.ajax({
+		method: 'GET',
+		url: url,
+		success: function(res) {
+			$paragraph.text(res.value.joke);
+		}
 	});
-	xhr.send();
 }
-
-getJoke();
